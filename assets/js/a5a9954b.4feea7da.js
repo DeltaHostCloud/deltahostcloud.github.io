846d1d4ed6064811f6a0cc617ac178a77ce2c8ea
lox1co <lox1co@users.noreply.github.com>
2023-01-15 06:41:14 +0000

    deploy: 78efa8758c7c437c082cd11ee1a84b211f7056a7

---
+++ assets/js/a5a9954b.4feea7da.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdeltadocs=self.webpackChunkdeltadocs||[]).push([[6677],{2681:a=>{a.exports=JSON.parse('{"title":"Caracter\xedsticas","slug":"/category/caracter\xedsticas","permalink":"/category/caracter\xedsticas","navigation":{"previous":{"title":"Introducci\xf3n","permalink":"/"},"next":{"title":"Consola","permalink":"/caracteristicas/consola"}}}')}}]);
---
+++ assets/js/eacdcf07.246a2407.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdeltadocs=self.webpackChunkdeltadocs||[]).push([[8785],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
